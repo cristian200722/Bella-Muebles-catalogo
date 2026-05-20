d07924921349c20915fe6f12d1e0fe2988f65d83
cristian200722 <cristianhernandez200722@gmail.com>
2026-05-20 11:36:29 -0500

    agregue imagenes optimizadas y ajuste los cards

--- script.js
+++ script.js
@@ -1,15 +1,62 @@
-const botones = document.querySelectorAll(".ver-mas");
+const botonesVerMas = document.querySelectorAll(".ver-mas");
 
-botones.forEach(boton=>{
-    boton.addEventListener("click",()=>{
-        const card = boton.closest(".card");
+botonesVerMas.forEach(boton => {
+
+    boton.addEventListener("click", () => {
+
+        // buscar SOLO dentro de esta card
+        const card = boton.parentElement;
+
         const info = card.querySelector(".info-producto");
 
-        if(!info) return;
+        // toggle
+        if(info.style.display === "block"){
 
-        info.style.display =
-        info.style.display==="block"
-        ? "none"
-        : "block";
+            info.style.display = "none";
+            boton.textContent = "Ver más";
+
+        }else{
+
+            info.style.display = "block";
+            boton.textContent = "Ver menos";
+
+        }
+
     });
+
+});
+
+
+
+/* ===== FILTRO CATEGORIAS ===== */
+
+const botones = document.querySelectorAll(".botones-categorias button");
+
+const productos = document.querySelectorAll(".card");
+
+botones.forEach(boton => {
+
+    boton.addEventListener("click", () => {
+
+        const filtro = boton.dataset.filtro;
+
+        productos.forEach(producto => {
+
+            if(
+                filtro === "todos" ||
+                producto.dataset.categoria === filtro
+            ){
+
+                producto.style.display = "block";
+
+            }else{
+
+                producto.style.display = "none";
+
+            }
+
+        });
+
+    });
+
 });
